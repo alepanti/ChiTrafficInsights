5996746cc854b0d596bc8b030994d1a65813cdb9
Alexis Pantig <alexispantig@gmail.com>
2025-03-26 20:47:39 +0000

    build dimensions, reformat fact

--- dbt/models/core/fct_traffic.sql
+++ dbt/models/core/fct_traffic.sql
@@ -1,15 +1,21 @@
 {{
     config(
-        materialized='incremental',
-        unique_key='record_id'
+        materialized="incremental",
+        unique_key="record_id",
+        partition_by={
+            "field": "date",
+            "data_type": "timestamp",
+            "granularity": "day",
+        },
+        cluster_by="region_id",
     )
 }}
 
-select * from {{ ref("stg_traffic_data") }}
+select record_id, region_id, date, speed
+from {{ ref("stg_traffic_data") }}
 
 {% if is_incremental() %}
 
-where
-  record_id not in (select record_id from {{ this }})
+    where record_id not in (select record_id from {{ this }})
 
 {% endif %}
