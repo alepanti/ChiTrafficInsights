0765cee56d57d31bfbdc055b3f1d254044a581bd
alepanti <alexispantig@gmail.com>
2025-03-27 14:17:30 +0000

    implement backfill start and end

--- api_load/api_load.py
+++ api_load/api_load.py
@@ -4,28 +4,34 @@ import csv
 from google.cloud import storage
 from datetime import datetime, timezone, timedelta
 
-# Chicago Data Portal API URL (CSV format)
+# Chicago Data Portal API URL
 API_URL = "https://data.cityofchicago.org/resource/kf7e-cur8.json"
 
 GCS_BUCKET_NAME = os.getenv('GCS_BUCKET_NAME')
 GCS_DEST_PATH = os.getenv('CSV_FILE_PATH')
 
 # Backfill configuration
-BACKFILL_DATE = os.getenv("BACKFILL_DATE", "2025-01-01T00:00:00.000")
-start_date = BACKFILL_DATE or (datetime.now(timezone.UTC) - timedelta(days=1)).strftime("%Y-%m-%dT%H:%M:%S.000")
+backfill_start = os.getenv("START_DATE") or None
+backfill_end = os.getenv("END_DATE") or None
+since_date = (datetime.now(timezone.UTC) - timedelta(days=1)).strftime("%Y-%m-%dT%H:%M:%S.000")
 
 # Temporary file path to store the CSV locally before uploading to GCS
 TEMP_CSV_FILE = "/tmp/raw_data.csv"
 
-def fetch_paginated_traffic_data(since_time):
+def fetch_paginated_traffic_data(since_time=None, start_time=None, end_time=None):
+    
+    if since_time:
+        url = f"https://data.cityofchicago.org/resource/kf7e-cur8.json?$where=time>'{since_time}'&$order=time DESC"
+    else:
+        url = f"https://data.cityofchicago.org/resource/kf7e-cur8.json?$where=time>'{start_time}' AND time<'{end_time}'&$order=time DESC"
+
+    print(f"Fetching data from: {url}") 
+
     offset = 0
     limit = 1000 
     all_json_data = []
 
-    print(f"Fetching data from API")
     while True:
-
-        url = f"{API_URL}?$where=time>'{since_time}'&$limit={limit}&$offset={offset}&$order=time DESC"
         
         response = requests.get(url)
         response.raise_for_status()
@@ -61,10 +67,12 @@ def upload_to_gcs(json_data):
     print(f"File successfully uploaded to gs://{GCS_BUCKET_NAME}/{GCS_DEST_PATH}")
 
 def main():
-    last_timestamp = BACKFILL_DATE or start_date
-    print(f"Fetching data since {last_timestamp}")
-    
-    json_data = fetch_paginated_traffic_data(last_timestamp)
+    if backfill_start is None:
+        json_data = fetch_paginated_traffic_data(since_time=since_date)
+        print(f"Fetching data since {since_date}")
+    else:
+        json_data = fetch_paginated_traffic_data(start_time=backfill_start, end_time=backfill_end)
+        print(f"Fetching data between {backfill_start} and {backfill_end}")
 
     if json_data:
         upload_to_gcs(json_data)
