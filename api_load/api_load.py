434fc9cd8e2decbb96586a2b7ea233ca4db62a5b
alepanti <alexispantig@gmail.com>
2025-03-27 15:13:31 +0000

    sfg sfg

--- api_load/api_load.py
+++ api_load/api_load.py
@@ -20,9 +20,11 @@ backfill_end_utc = os.getenv("END_DATE") or None
 since_date_utc = (datetime.now(timezone.utc) - timedelta(24))
 
 #convert to central timezone
-backfill_start = datetime.strptime(backfill_start_utc, '%Y-%m-%dT%H:%M:%S.000').astimezone(central_tz).strftime("%Y-%m-%dT%H:%M:%S.000")
-backfill_end = datetime.strptime(backfill_end_utc, '%Y-%m-%dT%H:%M:%S.000').astimezone(central_tz).strftime("%Y-%m-%dT%H:%M:%S.000")
-since_date = since_date_utc.astimezone(central_tz).strftime("%Y-%m-%dT%H:%M:%S.000")
+if backfill_start_utc is not None:
+    backfill_start = datetime.strptime(backfill_start_utc, '%Y-%m-%dT%H:%M:%S.000').astimezone(central_tz).strftime("%Y-%m-%dT%H:%M:%S.000")
+    backfill_end = datetime.strptime(backfill_end_utc, '%Y-%m-%dT%H:%M:%S.000').astimezone(central_tz).strftime("%Y-%m-%dT%H:%M:%S.000")
+else:
+    since_date = since_date_utc.astimezone(central_tz).strftime("%Y-%m-%dT%H:%M:%S.000")
 print(f"{backfill_start}, {backfill_end}, {since_date}")
 
 # Temporary file path to store the CSV locally before uploading to GCS
