acae2aa7e93fe3b27003b2e13ce232fda32de073
alepanti <alexispantig@gmail.com>
2025-03-27 18:49:27 +0000

    change avg table model

--- dbt/models/core/fct_avg_speed_by_region_hour.sql
+++ dbt/models/core/fct_avg_speed_by_region_hour.sql
@@ -1,11 +1,11 @@
 {{ config(materialized="table") }}
 
 select
-    d.date,
     r.region_id,
     d.hour,
-    avg(f.speed) as avg_speed
+    avg(f.speed) as avg_speed,
+    count(*) as data_points 
 from {{ ref("fct_traffic") }} f
 join {{ ref("dim_date") }} d on f.date = d.date
 join {{ ref("dim_region") }} r on f.region_id = r.region_id
-group by d.date, r.region_id, d.hour
+group by 1, 2
